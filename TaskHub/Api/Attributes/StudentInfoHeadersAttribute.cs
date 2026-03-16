4b332df0588348b3573b8e15d0d260323bccc009
F0ge1 <Kirmur733@yandex.ru>
2026-03-16 15:51:41 +0500

    add attributes to UsersController

---
+++ TaskHub/Api/Attributes/StudentInfoHeadersAttribute.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Api.Attributes;
+
+/// <summary>
+/// Атрибут для добавления заголовков с информацией о студенте
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public class StudentInfoHeadersAttribute : Attribute, IResultFilter
+{
+    private const string NameHeader = "X-Student-Name";
+    private const string GroupHeader = "X-Student-Group";
+
+    private readonly string _studentName;
+    private readonly string _studentGroup;
+
+    public StudentInfoHeadersAttribute(string studentName, string studentGroup)
+    {
+        _studentName = studentName;
+        _studentGroup = studentGroup;
+    }
+
+    public void OnResultExecuting(ResultExecutingContext context)
+    {
+        context.HttpContext.Response.OnStarting(() =>
+        {
+            context.HttpContext.Response.Headers.TryAdd(NameHeader, _studentName);
+            context.HttpContext.Response.Headers.TryAdd(GroupHeader, _studentGroup);
+
+            return Task.CompletedTask;
+        });
+    }
+
+    public void OnResultExecuted(ResultExecutedContext context)
+    {
+        // Ничего не делаем
+    }
+}
---
+++ TaskHub/Api/Attributes/ValidateUserRequestAttribute.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Reflection;
+
+namespace Api.Attributes;
+
+/// <summary>
+/// Атрибут для валидации запросов пользователя
+/// </summary>
+[AttributeUsage(AttributeTargets.Method)]
+public class ValidateUserRequestAttribute : Attribute, IActionFilter
+{
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        foreach (var argument in context.ActionArguments)
+        {
+            if (argument.Value == null)
+            {
+                context.Result = new BadRequestObjectResult("Тело запроса отсутствует");
+                return;
+            }
+
+            var nameProperty = argument.Value.GetType().GetProperty("Name", BindingFlags.Public | BindingFlags.Instance);
+
+            if (nameProperty != null)
+            {
+                var nameValue = nameProperty.GetValue(argument.Value) as string;
+
+                if (string.IsNullOrWhiteSpace(nameValue))
+                {
+                    context.Result = new BadRequestObjectResult("Имя пользователя не задано");
+                    return;
+                }
+            }
+        }
+    }
+
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+        // Ничего не делаем
+    }
+}
